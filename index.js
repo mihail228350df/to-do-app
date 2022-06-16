8168d5f89cc224b0bd132d7d22aaa98a2712e4e7
Mihail Sorokin <85826837+mihail228350df@users.noreply.github.com>
2022-06-16 21:01:27 +0300

    to do

---
+++ index.js
@@ -0,0 +1,54 @@
+const todosNode = document.querySelector('.js-todos');
+const inputNode = document.querySelector('.js-input');
+const btnNode = document.querySelector('.js-btn'); 
+let todos = [];
+
+function addTodo(text) {
+    const todo = {
+        text,
+        done: false,
+        id: `${Math.random()}`
+    };
+    
+    todos.push(todo);
+}
+
+function deleteTodo(id) {
+    todos.forEach(todo => {
+        if (todo.id === id)  {
+            todo.done = true;
+        }
+    })
+}
+
+function render() {
+    console.log(todos);
+   let html = '';
+   todosNode.forEach( => {
+    if (todo.done) {
+        return;
+    };
+
+      html += `
+        <div>
+            ${todo.text}
+            <button data-id='${todo.id}'>Сделано</button>
+        </div>`;
+   })
+
+
+    
+   
+    todosNode.innerHTML = html;
+}
+
+ btnNode.addEventListener('click' () => {
+    const text = inputNode.value;
+
+    addTodo(text);
+    
+    render();
+ });
+todosNode.addEventListener('click', () =>)
+
+render();
